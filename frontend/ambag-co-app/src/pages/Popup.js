4722558b7cfa71e0bcc53b0d33f96588ca74d888
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2021-12-08 17:46:22 +0700

    Modify popup component

---
+++ frontend/ambag-co-app/src/pages/Popup.js
@@ -0,0 +1,66 @@
+
+import React from 'react';
+import { useState } from 'react';
+import PostApi from '../apis/Post-api';
+
+function Popup({closePopup}) {
+
+  const[itemName, setItemName] = useState('');
+  const[itemPrice, setItemPrice] = useState(0);
+  const[expiry, setExpiry] = useState();
+  const[charity, setCharity] = useState('');
+  const[description, setDescription] = useState('');
+
+  const handlePost = (e) =>{
+    e.preventDefault()
+    new Promise((resolve, reject) => {
+      const response = PostApi.post('/postItem', {
+      item_name: itemName,
+      item_price: itemPrice,
+      item_exp_date: expiry,
+      item_charity: charity,
+      item_desc: description
+    })
+    resolve(response)
+  }).then((response) => console.log(response))
+  }
+
+  return (
+    <div>
+      <React.Fragment>
+          <button onClick={()=> closePopup(false)}>X</button>
+
+          <input type="text" placeholder="Item name" 
+          value = {itemName}
+          onChange={(e)=> setItemName(e.target.value)}>
+          </input>
+
+          <input type="text" placeholder="Item price" 
+          value = {itemPrice} 
+          onChange={(e)=> setItemPrice(e.target.value)}>
+          </input>
+
+          <input type="date" placeholder="Expiry date" 
+          value = {expiry} 
+          onChange={(e)=> setExpiry(e.target.value)}>
+          </input>
+
+          <input type="text" placeholder="Charity" 
+          value = {charity} 
+          onChange={(e)=> setCharity(e.target.value)}>
+          </input>
+
+          <input type="text" placeholder="description"
+           value = {description} 
+           onChange={(e)=> setDescription(e.target.value)}>
+           </input>
+
+           <button onClick={handlePost} type="submit">Post</button>
+      </React.Fragment>
+    </div>
+  )
+}
+
+export default Popup;
+
+
