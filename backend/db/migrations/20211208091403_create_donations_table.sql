49e426bc88f99163e734b65d25083ee1ad83e7a7
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2021-12-08 16:55:21 +0700

    Modify migration for donations

--- backend/db/migrations/20211208091403_create_donations_table.sql
+++ backend/db/migrations/20211208091403_create_donations_table.sql
@@ -1,9 +1,12 @@
 -- migrate:up
+CREATE TYPE d_stat AS ENUM('pending', 'done');
+
 CREATE TABLE donations (
     don_id BIGSERIAL PRIMARY KEY,
     char_id INT NOT NULL REFERENCES charities(char_id),
     don_amount NUMERIC NOT NULL,
-    don_dot DATE NOT NULL
+    don_dot DATE NOT NULL,
+    don_status d_stat NOT NULL
 );
 
 -- migrate:down
