91e357a78dac47e47912c7db0f57f3e3c7c614c9
karms lajo <karms.lajo@gmail.com>
2021-12-10 11:33:37 +0800

    add edit item popup dialog

--- frontend/ambag-co-app/src/views/my-shop/MyItemCard.js
+++ frontend/ambag-co-app/src/views/my-shop/MyItemCard.js
@@ -1,10 +1,12 @@
 import { Card, NavDropdown } from "react-bootstrap";
-import './MyItemCard.css';
-import DeleteItemPopup from "./DeleteItemPopup";
 import { useState } from "react";
+import DeleteItemPopup from "./DeleteItemPopup";
+import EditItemPopup from "./EditItemPopup";
+import './MyItemCard.css';
 
 export function MyItemCard(props) {
     const [DeleteButton, setDeleteButton] = useState(false);
+    const [EditButton, setEditButton] = useState(false);
 
     return (
         <div className="col-5 col-md-3 col-lg-3">
@@ -14,7 +16,8 @@ export function MyItemCard(props) {
                         <NavDropdown title={
                             <span><i className='fas fa-ellipsis-v'></i></span>} align="end">
                             <NavDropdown.Item>
-                                <button className="btn edit-btn">Edit</button>
+                                <button className="btn edit-btn"
+                                    onClick={()=> setEditButton(true)}>Edit</button>
                             </NavDropdown.Item>
                             <NavDropdown.Item>
                                 <button className="btn delete-btn" 
@@ -31,6 +34,7 @@ export function MyItemCard(props) {
                     <Card.Subtitle>P{props.price}</Card.Subtitle>
                 </Card.Body>
             </Card>
+            {EditButton && <EditItemPopup editButton={setEditButton} />}
             {DeleteButton && <DeleteItemPopup deleteButton={setDeleteButton} />}
         </div>
     );
