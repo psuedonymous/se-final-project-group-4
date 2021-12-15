b369c2b5868157ac6fb37be6bd0c8487fa602966
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2021-12-15 17:08:54 +0700

    Modify edit page to edit an item

--- frontend/ambag-co-app/src/views/my-shop/EditItemPopup.js
+++ frontend/ambag-co-app/src/views/my-shop/EditItemPopup.js
@@ -1,37 +1,69 @@
 import Popup from "../../components/Popup";
 import "./EditItemPopup.css";
+import {useState} from 'react';
 
-export default function EditItemPopup({ editButton }) {
+export default function EditItemPopup({ editButton, itemId, theItem }) {
+
+
+  const[itemName, setItemName] = useState(theItem.item_name);
+  const[itemPrice, setItemPrice] = useState(theItem.item_price);
+  const[itemDesc, setItemDesc] = useState(theItem.item_desc);
+  const[itemExp, setItemExp] = useState(theItem.item_exp_date);
+
+  const editItem = e =>{
+    e.preventDefault();
+
+    new Promise((resolve,reject)=>{
+      const body = {itemName, itemPrice, itemDesc, itemExp}
+      const response = fetch(`http://localhost:5000/getItems/${itemId}`,{
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      })
+      resolve(response);
+      reject("Failed to edit");
+      window.location ="/my-shop";
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }
+
     return (
         <div>
             <Popup >
                 <button className="btn close-btn" onClick={() => editButton(false)}>X</button>
                 <div className="row">
                     <div className="col-4">
-                        <di className="row">
+                        <div className="row">
                             <img src="https://www.qoobee.com/wp-content/uploads/QooBee-Stuffed-Toy-2019.jpg" width={150} height={150}></img>
                             <input className="mt-2" type="file"></input>
-                        </di>
+                        </div>
                     </div>
                     <div className="col-8">
                         <div className="row">
-                            <div className="col-6">
-                                <input className="form-control" type="text" placeholder="Name"></input>
+                            <div className="col-12">
+                                <input className="form-control" value={itemName} onChange={(e)=>{setItemName(e.target.value)}} type="text" placeholder="Name"></input>
                             </div>
-                            <div className="col-6">
-                                <input className="form-control" type="text" placeholder="Price"></input>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <input className="form-control mt-2" value={itemPrice} onChange={(e)=>{setItemPrice(e.target.value)}} type="text" placeholder="Price"></input>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <input className="form-control mt-2" value={itemExp} onChange={(e)=>{setItemExp(e.target.value)}} type="Date" placeholder="Expiry"></input>
                             </div>
                         </div>
                         <select className="form-control mt-2" name="charity">
-                            <option value="select-charity">Select Charity</option>
-                            <option value="abs">ABS-CBN Handog Kapamilya</option>
-                            <option value="tawili">Tawili University</option>
+                            <option value="select-charity">Share an Opportunity</option>
                         </select>
-                        <textarea className="form-control mt-2" placeholder="Description" maxlength={150}></textarea>
+                        <textarea className="form-control mt-2" value={itemDesc} onChange={(e)=>{setItemDesc(e.target.value)}} placeholder="Description" maxLength={150}></textarea>
                     </div>
                 </div>
                 <div className="row mt-2 m-1">
-                    <button className="save-btn col-3 ms-auto">Save</button>
+                    <button className="save-btn col-3 ms-auto" onClick={(e) => {editItem(e); editButton(false)}}>Save</button>
                 </div>
             </Popup>
         </div>
