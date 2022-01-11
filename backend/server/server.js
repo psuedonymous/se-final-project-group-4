31c4cce5203af41df90b1014a282a8ab4f044ca0
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2022-01-11 15:01:23 +0700

    Add api for posting new item with image

--- backend/server/server.js
+++ backend/server/server.js
@@ -32,65 +32,6 @@ app.get('/getItems', (req, res) => {
     })
 })
 
-//endpoint for getting images from cloudinary [not yet working]
-app.get('/getImages', (req,res)=>{
-  new Promise((resolve,reject)=>{
-    const { resources } = cloudinary.search
-    .expression('folder:ambag_co')
-    .execute().then((resources) =>{
-      res.send(resources)
-    })
-  })
-  .catch((err)=>{ 
-    console.error(err);
-  })
-
-})
-
-//endpoint for uploading images to cloudinary
-app.post('/uploadImage', (req,res)=>{
-  new Promise((resolve,reject)=>{
-    const fileStr = req.body.data;
-    const uploadResponse = cloudinary.uploader.upload(fileStr,{
-       upload_preset: 'ambag_co'
-    })
-    resolve(uploadResponse);
-    reject("Failed to upload an image");
-  })
-  .then((uploadResponse)=> 
-  console.log(uploadResponse)
-  )
-  .catch((err) => {
-    console.error(err);
-  })
-
-  res.json({message:"uploaded successfully"});
-})
-
-//endpoint for posting a new item
-app.post('/postItem', (req,res) => {
-  new Promise((resolve, reject) => {
-    const result = db.query
-    ("INSERT INTO donations(char_id, don_amount, don_dot) VALUES($1, $2, $3) RETURNING *",
-    [req.body.item_charity, req.body.item_price, req.body.don_dot]);
-    resolve(result);
-    reject("Failed to post an item")
-  })
-  .then((result) => {
-    const result2 = db.query
-    ("INSERT INTO items(cat_id, shop_id, don_id, item_name, item_price, item_desc, item_exp_date, item_date_posted) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-    [req.body.item_charity, req.body.shop_id, result.rows[0].don_id , req.body.item_name,req.body.item_price,req.body.item_desc,
-    req.body.item_exp_date, req.body.item_date_posted]);
-  })
-  .catch((err)=>{
-    console.error(err);
-  })
-
-  res.status(200).json({
-    status: "success"
-  })
-})
-
 
 //endpoint for deleting an item
 app.delete("/getItems/:id", (req,res)=>{
@@ -131,6 +72,44 @@ app.put("/getItems/:id", (req, res)=>{
 
   res.json("Item was edited!")
 })
+
+
+//persisting-image
+app.post("/post-item", (req, response) => {
+  // collected image from a user
+  const data = {
+    image: req.body.image
+  }
+ 
+  // upload image here
+  cloudinary.uploader.upload(data.image,{
+    upload_preset: 'ambag_co'
+ })
+  .then((image) => {
+    new Promise((resolve, reject) => {
+      const result = db.query("INSERT INTO items(cat_id, shop_id, item_name, item_price, item_desc, item_exp_date, item_date_posted, char_id, item_image) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+      [req.body.item_charity, req.body.shop_id , req.body.item_name ,req.body.item_price , req.body.item_desc,
+      req.body.item_exp_date , req.body.item_date_posted , req.body.char, image.secure_url]);
+      resolve(result)   
+    }).then((result)=>console.log(result.rows[0]))
+  }).then(
+    response.status(201).send({
+      status: "success",
+      data: {
+        message: "Image Uploaded Successfully",
+      },
+    })
+  )
+  .catch((error) => {
+    response.status(500).send({
+      message: "failure",
+      error,
+    });
+  });
+});
+
+
+
 
 app.listen(port, ()=> {
   console.log(`Server is running on port ${port}`);
