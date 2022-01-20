d81a408a3277bbc47387cff9deafb3f237ab1171
karms lajo <karms.lajo@gmail.com>
2022-01-20 19:59:17 +0800

    display empty bag if no items added

--- frontend/ambag-co-app/src/views/ShopBagPage.js
+++ frontend/ambag-co-app/src/views/ShopBagPage.js
@@ -2,44 +2,63 @@ import { NavBar } from "../components/NavBar";
 import ShopBagItemCard from "../components/ShopBagItemCard";
 import { useState, useEffect } from "react";
 import { displayShopBagItems } from "../apis/Get-apis";
+import { useNavigate } from "react-router-dom";
 import './Shopbag.css';
 
 export default function ShopBagPage(props) {
   const [items, setItems] = useState([]);
   const [checkedItems, setCheckedItems] = useState([]);
 
+  const navigate = useNavigate();
+
+  const navigateToShop = (e) => {
+    e.preventDefault();
+    navigate('/catalog')
+  }
+
   useEffect(() => {
     displayShopBagItems(setItems);
   })
 
 
-    return (
-        <>
-            <NavBar/>
-
-            <section className="py-4 container" >
-              <div class="row">
-              </div>
-              <div className="row justify-content-right">
-                  {items.map((item) => {
-                      return ( 
-                      <ShopBagItemCard
-                        id = {item.i_id}
-                        img={item.i_image} 
-                        title={item.i_name} 
-                        desc={item.i_desc} 
-                        price={item.i_price} 
-                        checkedItems= {checkedItems}
-                        setCheckedItems={setCheckedItems} />)
-                  })}
-              </div>
-          </section>
-
-          <section>
-            <div className="col-md-11">
-            <button type="button" className="float-end mb-5 checkout-button">Checkout</button>
+  return (
+    <>
+      <NavBar />
+      {items.length === 0 ?
+        <div className='container'>
+          <div className='status-container'>
+            <div className='d-flex justify-content-center align-items-center empty-bag-status'>
+              <div className="col-2">Your bag is empty.</div>
+              <button className="btn shop-btn"
+                onClick={navigateToShop}>
+                Shop here
+              </button>
+            </div>
+          </div>
+        </div> : <div>
+          <section className="py-4 container" >
+            <div class="row">
+            </div>
+            <div className="row justify-content-right">
+              {items.map((item) => {
+                return (
+                  <ShopBagItemCard
+                    id={item.i_id}
+                    img={item.i_image}
+                    title={item.i_name}
+                    desc={item.i_desc}
+                    price={item.i_price}
+                    checkedItems={checkedItems}
+                    setCheckedItems={setCheckedItems} />)
+              })}
             </div>
           </section>
-        </>
-    )
+          <section>
+            <div className="col-md-11">
+              <button type="button" className="float-end mb-5 checkout-button">Checkout</button>
+            </div>
+          </section>
+        </div>}
+    </>
+  )
 }
